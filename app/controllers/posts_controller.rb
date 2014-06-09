775cfc55d3abe24c2f688bfe223dffaff2f5fe51
erdnusskeks <erdnusskeks@googlemail.com>
2014-06-09 11:46:26 +0200

    edit delete and categories

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,14 +8,42 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
     redirect_to posts_path
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, :category)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+
+  CATEGORIES = [
+    ["awesome", "awesome"],
+    ["hilarious", "hilarious"],
+    ["sweet", "sweet"],
+    ["yamyam", "yamyam"],
+    ["oh no!", "oh no!"],
+    ["not my day...", "not my day..."]
+  ]
+
 end
